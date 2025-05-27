cd14ab25a5d08ea7d2119157bdd44592b9fdf3f3
egoricsss <egor-syrov@bk.ru>
2025-05-27 09:01:08 +0300

    update docker-compose.yml

--- src/core/config/config.py
+++ src/core/config/config.py
@@ -7,8 +7,8 @@ class BotConfig(BaseModel):
     BOT_TOKEN: str = os.getenv("BOT_TOKEN")
     WEBHOOK_URL: str = os.getenv("WEBHOOK_URL")
     SECRET: str = os.getenv("SECRET")
-    WEBHOOK_PATH: str = "/webhook"
-    CLEAR_LOGS:str = os.getenv("CLEAR_LOGS")
+    WEBHOOK_PATH: str = os.getenv("WEBHOOK_PATH")
+    CLEAR_LOGS: bool = os.getenv("CLEAR_LOGS")
 
 
 config = BotConfig()
